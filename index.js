fbde6ed61e861f940aea6fdb658666af3fabf33b
nn-tran <48101680+nn-tran@users.noreply.github.com>
2022-12-06 14:36:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//by TGM043 (Discord)/ironmaiden1872 (Nexus Mods)
+import cleaned from './BakaStrings.json' assert {type: 'json'}
+const download = (filename, text) => {
+	var pom = document.createElement('a')
+	pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+	pom.setAttribute('download', filename)
+	pom.click()
+	pom.remove()
+}
+const cleanJson = (old) => {
+	return {string: {...old.string, ...cleaned.string}}
+}
+let toSave = null;
+document.getElementById('file').addEventListener('input', async (e)=>{
+	try {
+		toSave = JSON.parse(await e.target.files[0].text())
+	} catch {}
+})
+document.getElementById('saver').addEventListener('click', async (e)=>{
+	try {
+		toSave = cleanJson(toSave)
+		if (!toSave) throw new Error()
+		download("BakaStrings.json", JSON.stringify(toSave, null, '\t'))
+		e.target.innerHTML = 'success'
+	} catch {
+		e.target.innerHTML = 'failed'
+		return
+	}
+})
